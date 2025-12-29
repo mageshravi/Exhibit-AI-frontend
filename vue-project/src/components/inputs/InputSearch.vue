812afb32ae3850dc21c3c6d43e9310078b001017
Magesh Ravi <magesh@webinative.com>
2025-12-29 08:19:27 +0530

    feat: [#14] InputSearch v-model and focus
    - adds func to focus the search input element and exposes it
    - adds support for v-model attribute

--- vue-project/src/components/inputs/InputSearch.vue
+++ vue-project/src/components/inputs/InputSearch.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 const props = defineProps<{
   placeholder?: string | undefined
+  value?: string | undefined
 }>()
+
+const inputRef = ref<HTMLInputElement | null>(null)
+
+const focus = () => {
+  inputRef.value?.focus()
+}
+
+defineExpose({
+  focus,
+})
 
 const emits = defineEmits<{
   (e: 'focus:in'): void
   (e: 'focus:out'): void
   (e: 'keyup', event: KeyboardEvent): void
+  (e: 'update:value', value: string): void
 }>()
 
 const exitSearch = (event: KeyboardEvent) => {
@@ -20,7 +34,10 @@ const exitSearch = (event: KeyboardEvent) => {
     <input
       type="search"
       class="m-searchbox__input"
+      ref="inputRef"
       :placeholder="props.placeholder"
+      :value="props.value"
+      @input="emits('update:value', ($event.target as HTMLInputElement)?.value ?? '')"
       @focusin="emits('focus:in')"
       @focusout="emits('focus:out')"
       @keyup.esc="exitSearch"
