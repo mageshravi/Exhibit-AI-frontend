1efdc3f4a7bad6419b12d35172d2252665d539f9
Magesh Ravi <magesh@webinative.com>
2026-01-24 21:30:54 +0530

    feat [#19]: Update EntitySelector
    - adds an optional remove-entity-callback that can veto the built-in remove-entity function.

--- vue-project/src/components/inputs/EntitySelector.vue
+++ vue-project/src/components/inputs/EntitySelector.vue
@@ -8,6 +8,7 @@ const props = defineProps<{
   addEntityCallback: () => void
   entities?: Map<string, Entity>
   errorText?: string
+  removeEntityCallback?: (value: Entity) => void
 }>()
 
 const state = reactive<{
@@ -21,8 +22,16 @@ const emits = defineEmits<{
 }>()
 
 const removeEntity = (value: string) => {
-  state.entities.delete(value)
-  emits('update:entities', state.entities)
+  if (props.removeEntityCallback) {
+    /* use callback instead of modifying entities directly */
+    const entity = state.entities.get(value)
+    if (entity) {
+      props.removeEntityCallback(entity)
+    }
+  } else {
+    state.entities.delete(value)
+    emits('update:entities', state.entities)
+  }
 }
 
 watch(
